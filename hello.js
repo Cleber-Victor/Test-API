5de93228cd707485366034f69eacf6eb804aa159
Cleber-victor <cvsj@cin.ufpe.br>
2026-02-19 01:15:09 -0300

    Create get and post to a blog API

---
+++ hello.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const z = require("zod");
+const app = express();
+app.use(express.json());
+const PORT = 3000;
+
+const Posts = [];
+
+app.listen(PORT, () => {
+  console.log(`Listening to ${PORT} `);
+});
+
+app.get("/api/posts", (req, res) => {
+  res.send(Posts);
+});
+
+app.get("/api/posts/:id", (req, res) => {
+  const achou = Posts.find((p) => p.id === parseInt(req.id));
+  if (!achou) {
+    return res.status(404).send("Post não existe");
+  }
+  res.send(achou);
+});
+
+const postsSchema = z.object({
+  title: z.string().min(3),
+  body: z.string().min(1),
+});
+
+app.post("/api/posts", (req, res) => {
+  // validar formato
+  const result = postsSchema.safeParse(req.body);
+
+  if (!result.success) {
+    return res.status(400).json({
+      message: "Dados inválidos",
+      errors: result.error.flatten().fieldErrors,
+    });
+  }
+
+  const post = {
+    id: Posts.length + 1,
+    title: req.body.title,
+    body: req.body.body,
+  };
+  Posts.push(post);
+  res.send(post);
+});
---
+++ server.js
@@ -0,0 +1,80 @@
+const Joi = require("joi");
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+const courses = [
+  { id: 1, name: "course1" },
+  { id: 2, name: "course2" },
+  { id: 3, name: "course3" },
+];
+
+app.listen(3000, () => {
+  console.log(`Listening on port 3000`);
+});
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get("/api/courses", (req, res) => {
+  res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) => {
+  //res.send(courses[req.params.id - 1]); minha implementação
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    // 404
+    res.status(404).send("The course with the given ID was not found");
+  }
+  res.send(course);
+});
+
+app.post("/api/courses", (req, res) => {
+  const result = validateCourse(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+
+  const course = {
+    id: courses.length + 1,
+    name: req.body.name,
+  };
+  courses.push(course);
+  res.send(course);
+});
+
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    // 404
+    res.status(404).send("The course with the given ID was not found");
+  }
+  const result = validateCourse(req.body);
+  if (result.error) {
+    res.status(400).send(result.error.details[0].message);
+    return;
+  }
+  course.name = req.body.name;
+  res.send(course);
+});
+
+function validateCourse(course) {
+  const schema = {
+    name: Joi.string().min(3).required(),
+  };
+  return Joi.validate(course, schema);
+}
+
+app.delete("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) {
+    // 404
+    return res.status(404).send("The course with the given ID was not found");
+  }
+  const index = courses.indexOf(course);
+  courses.splice(index, 1);
+  res.send(course);
+});
