7d7c9c5974975a8ba07d08ecfd628d99649a9d39
Cleber-victor <cvsj@cin.ufpe.br>
2026-03-13 12:55:39 -0300

    feat: creating validation with ZOD

---
+++ src/middlewares/validatePost.js
@@ -0,0 +1,25 @@
+import * as z from "zod"; 
+ 
+const Post = z.object({ 
+  title: z.string().min(1).max(100),
+  body: z.string().min(1).max(5000)
+});
+
+export const validatePost = (req, res, next) => {
+  try {
+    Post.parse(req.body);
+    next();
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({
+        status: 400,
+        message: "Dados de entrada inválidos",
+        errors: error.errors.map((err) => ({
+          campo: err.path[0],
+          mensagem: err.message,
+        })),
+      });
+    }
+    next(error);
+  }
+};
--- src/routes/postRoutes.js
+++ src/routes/postRoutes.js
@@ -7,13 +7,14 @@ import {
   updatePost,
   deletePost,
 } from "../controller/postController.js";
+import { validatePost } from "../middlewares/validatePost.js";
 
 const router = express.Router();
 
-router.post("/posts", createPost);
+router.post("/posts", validatePost, createPost);
 router.get("/posts", getAllPosts);
 router.get("/posts/:id", getPostById);
-router.put("/posts/:id", updatePost);
+router.put("/posts/:id", validatePost, updatePost);
 router.delete("/posts/:id", deletePost);
 
 export default router;
